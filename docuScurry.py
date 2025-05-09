9c5fb26225e986b922b0436b77a2a58856417fe9
ajpk <41717689+anthonyjk@users.noreply.github.com>
2025-05-08 21:29:52 -0400

    Scraper menu added

--- docuScurry.py
+++ docuScurry.py
@@ -1,10 +1,13 @@
 import sys
 import os
 
+#from dataScurrier import MuPDFScurrier
+
 from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtCore import QSize, Qt, QThread, pyqtSignal
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QMainWindow, QFileDialog, 
-	QProgressBar, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QSizePolicy, QGridLayout)
+	QProgressBar, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QSizePolicy, QStackedWidget, 
+	QCheckBox, QComboBox)
 
 class MainWindow(QMainWindow):
 	def __init__(self):
@@ -12,25 +15,32 @@ class MainWindow(QMainWindow):
 
 		self.setWindowTitle("DocuScurry")
 		self.setFixedSize(QSize(400, 220))
-		#setMinimumSize
-		#setMaximumSize
 
-		self.initUI()
+		self.stacked_widget = QStackedWidget()
+		self.main_window = QWidget()
+		self.scrape_window = QWidget()
+		self.viewer_window = QWidget()
 
-	def initUI(self):
-		w = QWidget()
-		self.setCentralWidget(w)
-		vbox = QVBoxLayout(w)
+		self.stacked_widget.addWidget(self.main_window)
+		self.stacked_widget.addWidget(self.scrape_window)
+		self.stacked_widget.addWidget(self.viewer_window)
+
+		self.initMainUI()
+		self.initScraperUI()
+
+		self.setCentralWidget(self.stacked_widget)
+		self.stacked_widget.setCurrentIndex(0)
+
+	def initMainUI(self):
+		vbox = QVBoxLayout(self.main_window)
 
 		# Buttons
-		self.scrape_button = QPushButton("Scraper")
-		self.scrape_button.setCheckable(True)
-		self.scrape_button.setFixedSize(180,65)
-		#self.scrape_button.clicked.connect(self.directory_scrape)
+		scrape_button = QPushButton("Scraper")
+		scrape_button.setFixedSize(180,65)
+		scrape_button.clicked.connect(self.switchScrape)
 
-		self.viewer_button = QPushButton("Viewer")
-		self.viewer_button.setCheckable(True)
-		self.viewer_button.setFixedSize(180,65)
+		viewer_button = QPushButton("Viewer")
+		viewer_button.setFixedSize(180,65)
 		#self.viewer_button.clicked.connect(self.file_scrape)
 
 		pixmap = QPixmap("./cerl_sqrl.png")
@@ -49,9 +59,8 @@ class MainWindow(QMainWindow):
 
 		smaller_vbox = QVBoxLayout()
 		smaller_vbox.setContentsMargins(10, 10, 10, 10)
-		smaller_vbox.addWidget(self.scrape_button, 0, Qt.AlignLeft | Qt.AlignCenter)
-		smaller_vbox.addWidget(self.viewer_button, 0, Qt.AlignLeft | Qt.AlignCenter)
-
+		smaller_vbox.addWidget(scrape_button, 0, Qt.AlignLeft | Qt.AlignCenter)
+		smaller_vbox.addWidget(viewer_button, 0, Qt.AlignLeft | Qt.AlignCenter)
 
 		hbox = QHBoxLayout()
 		#hbox.setContentsMargins(10, 10, 10, 10)
@@ -62,6 +71,122 @@ class MainWindow(QMainWindow):
 		vbox.addLayout(hbox)
 
 		vbox.addStretch(1)
+
+	def initScraperUI(self):
+		vbox = QVBoxLayout(self.scrape_window)
+
+		# Label
+		title_font = QFont()
+		title_font.setPointSize(20)
+		head_label = QLabel("DocuScraper")
+		head_label.setFont(title_font)
+
+		# Settings
+		ocr_check = QCheckBox("Image OCR Scanning")
+
+		ref_check = QCheckBox("Include References")
+
+		table_label = QLabel("Table Extraction Mode")
+		table_dropdown = QComboBox()
+		table_dropdown.addItem("None")
+		table_dropdown.addItem("Textual")
+		table_dropdown.addItem("Data")
+
+		# Start Scrape Buttons & Label
+		scrape_font = QFont()
+		scrape_font.setPointSize(12)
+		scrape_label = QLabel("Scrape Method")
+		scrape_label.setFont(scrape_font)
+
+		self.directory_button = QPushButton("Directory")
+		self.directory_button.clicked.connect(self.directoryScrape)
+		self.directory_button.setFixedSize(180,65)
+
+		self.file_button = QPushButton("File")
+		self.file_button.clicked.connect(self.fileScrape)
+		self.file_button.setFixedSize(180,65)
+
+		# Add all elements to widget
+		vbox.setSpacing(10)
+
+		# UPPER SECTION
+		left_vbox = QVBoxLayout()
+		left_vbox.addWidget(ocr_check, 0, Qt.AlignCenter | Qt.AlignTop)
+		left_vbox.addWidget(ref_check, 0, Qt.AlignCenter | Qt.AlignTop)
+
+		right_vbox = QVBoxLayout()
+		right_vbox.addWidget(table_label, 0, Qt.AlignCenter | Qt.AlignTop)
+		right_vbox.addWidget(table_dropdown, 0, Qt.AlignCenter | Qt.AlignTop)
+
+		hbox_top = QHBoxLayout()
+		hbox_top.addLayout(left_vbox)
+		hbox_top.addLayout(right_vbox)
+
+		# LOWER SECTION
+		hbox_bottom = QHBoxLayout()
+		hbox_bottom.addWidget(self.directory_button, 0, Qt.AlignCenter)
+		hbox_bottom.addWidget(self.file_button, 0, Qt.AlignCenter)
+
+		#vbox.addWidget(back_button, 0, Qt.AlignLeft)
+		vbox.addWidget(head_label, 0, Qt.AlignCenter | Qt.AlignTop)
+
+		vbox.addLayout(hbox_top)
+		vbox.addWidget(scrape_label, 0, Qt.AlignCenter)
+		vbox.addLayout(hbox_bottom)
+
+		vbox.addStretch(1)
+
+		# Return Button
+		back_button = QPushButton("Return", self.scrape_window)
+		back_button.setFixedSize(50,30)
+		back_button.clicked.connect(self.returnMain)
+		back_button.setGeometry(10, 10, 50, 30)
+		back_button.raise_()
+		#self.scrape_button.clicked.connect(self.switchScrape)
+
+	def directoryScrape(self):
+		options = QFileDialog.Options()
+
+		home_directory = os.path.expanduser("~")
+		directory = QFileDialog.getExistingDirectory(self,
+		"Select Directory", home_directory, options=options)
+
+		if directory:
+			directory += "/*.pdf"
+			print(directory)
+			self.processDocuments(directory)
+
+	def fileScrape(self): # change to file later
+		options = QFileDialog.Options()
+		home_directory = os.path.expanduser("~")
+
+		fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", home_directory,"PDF Files (*.pdf)", options=options)
+		if fileName:
+			self.processDocuments(fileName) 
+
+	def processDocuments(self, path):
+		self.directory_button.setEnabled(False) # Turn off buttons
+		self.file_button.setEnabled(False)
+
+		#self.scurrier = MuPDFScurrier(path)
+		#elf.scurrier.progress.connect(self.update_progress)
+		#self.scurrier.finished.connect(self.processing_finished)
+		#self.scurrier.current_pdf.connect(self.pass_current_pdf)
+		#self.scurrier.start()
+
+	def initViewerUI(self):
+		pass
+
+	def returnMain(self):
+		self.setFixedSize(QSize(400, 220))
+		self.stacked_widget.setCurrentIndex(0)
+
+	def switchScrape(self):
+		self.setFixedSize(QSize(400, 400))
+		self.stacked_widget.setCurrentIndex(1)
+
+	def switchViewer(self):
+		self.stacked_widget.setCurrentIndex(2)
 
 app = QApplication(sys.argv)
 
